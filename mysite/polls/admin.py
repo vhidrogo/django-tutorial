7232b2fb3b8841d673164286e72b6e9467451ba9
Victor Hidrogo <victor@nav.it>
2023-05-17 13:36:56 -0700

    feat: customize the admin change list

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,5 +12,12 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']})
     ]
     inlines = [ChoiceInline]
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+
+    # adds a “Filter” sidebar that lets people filter the change list by the pub_date field:
+    list_filter = ['pub_date']
+
+    # adds a search box at the top of the change list
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
